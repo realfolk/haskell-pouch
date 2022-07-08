b8455b41b8b27846dee5a057d78c66b70ca31fcb
Dhruv Dang <dhruv@realfolk.com>
2022-07-07 23:24:49 -0700

    lib/lib: Refactor Lib.Number.Word* modules by wrapping types with newtype wrappers to avoid instance collisions

--- src/lib/lib/Lib/Number/Word128.hs
+++ src/lib/lib/Lib/Number/Word128.hs
@@ -1,11 +1,32 @@
 module Lib.Number.Word128
-    ( module Basement.Types.Word128
+    ( Word128
     ) where
 
-import           Basement.Types.Word128 (Word128 (Word128))
-import           Data.Binary            (Binary)
-import qualified Data.Binary            as Binary
+import qualified Basement.Numerical.Number as Number
+import qualified Basement.Types.Word128    as Word128
+import           Data.Binary               (Binary)
+import qualified Data.Binary               as Binary
+import           Data.Bits                 (Bits)
+import           Data.Ratio                ((%))
+import           Foreign.Storable          (Storable)
+
+-- | Wrap the upstream 'Word128.Word128' type from basement
+-- to avoid any typeclass instance collisions.
+newtype Word128
+  = Word128 Word128.Word128
+  deriving (Bits, Bounded, Enum, Eq, Num, Ord, Show, Storable)
 
 instance Binary Word128 where
-  get = Word128 <$> Binary.get <*> Binary.get
-  put (Word128 a b) = Binary.put a >> Binary.put b
+  get = do
+    a <- Binary.get
+    b <- Binary.get
+    return $ Word128 $ Word128.Word128 a b
+  put (Word128 (Word128.Word128 a b)) = Binary.put a >> Binary.put b
+
+instance Real Word128 where
+  toRational (Word128 w) = Number.toInteger w % 1
+
+instance Integral Word128 where
+  toInteger (Word128 w) = Number.toInteger w
+  quotRem (Word128 a) (Word128 b) = (Word128 (Word128.quot a b), Word128 (Word128.rem a b))
+
--- src/lib/lib/Lib/Number/Word256.hs
+++ src/lib/lib/Lib/Number/Word256.hs
@@ -1,23 +1,34 @@
 module Lib.Number.Word256
-    ( module Basement.Types.Word256
+    ( Word256
     ) where
 
 import qualified Basement.Numerical.Number as Number
-import           Basement.Types.Word256    (Word256 (Word256))
 import qualified Basement.Types.Word256    as Word256
 import           Data.Binary               (Binary)
 import qualified Data.Binary               as Binary
+import           Data.Bits                 (Bits)
 import           Data.Ratio                ((%))
+import           Foreign.Storable          (Storable)
 
+-- | Wrap the upstream 'Word256.Word256' type from basement
+-- to avoid any typeclass instance collisions.
+newtype Word256
+  = Word256 Word256.Word256
+  deriving (Bits, Bounded, Enum, Eq, Num, Ord, Show, Storable)
 
 instance Binary Word256 where
-  get = Word256 <$> Binary.get <*> Binary.get <*> Binary.get <*> Binary.get
-  put (Word256 a b c d) = Binary.put a >> Binary.put b >> Binary.put c >> Binary.put d
+  get = do
+    a <- Binary.get
+    b <- Binary.get
+    c <- Binary.get
+    d <- Binary.get
+    return $ Word256 $ Word256.Word256 a b c d
+  put (Word256 (Word256.Word256 a b c d)) = Binary.put a >> Binary.put b >> Binary.put c >> Binary.put d
 
 instance Real Word256 where
-  toRational w = Number.toInteger w % 1
+  toRational (Word256 w) = Number.toInteger w % 1
 
 instance Integral Word256 where
-  toInteger = Number.toInteger
-  quotRem a b = (Word256.quot a b, Word256.rem a b)
+  toInteger (Word256 w) = Number.toInteger w
+  quotRem (Word256 a) (Word256 b) = (Word256 (Word256.quot a b), Word256 (Word256.rem a b))
 
